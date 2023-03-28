3ef4f93968eb3fa9f6543352e926376ebd2c6d14
Elizabeth Alabi <elizabeth200079@gmail.com>
2023-03-28 19:14:16 +0100

    Complete getters and Setters for all the properties

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,4 +1,69 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.time.LocalDate;
+
 public class BankAccount {
-}
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private int accountNumber;
+    private int balance;
+
+//Construction
+    public BankAccount(String inputFirstName, String inputLastname,
+                       String inputDob, int inputAccountnumber, int inputBalance){
+
+        this.firstName = inputFirstName;
+        this.lastName = inputLastname;
+        this.accountNumber = inputAccountnumber;
+        this.balance = inputBalance;
+
+
+
+    }
+//Getter and Setter
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastname() {
+            return lastName;
+        }
+    public int getAccount() {
+        return accountNumber;
+
+    public double getBalance() {
+        return balance;
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,18 +1,14 @@
+import org.example.BankAccount;
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.in;
 
 public class BankAccountTest {
-
-    @Test
-    void BankAccountTest() {
-        assertThat("actual").isEqualTo("expected");
-//        Given some input value(s).
-          String input = "Hello";
-//        When a certain operation is performed (method is called).
-
-//        Then expect a certain result.
+    public static void main(String[] args) {
+//        Testing
     }
+
 }
 
